54167a2e13cd0381eeb57f2bfeb7eb0bb4ab7889
mate0021 <mami.ext@gmail.com>
2021-12-05 12:28:33 +0100

    Cleanup.

--- src/main/java/rnd/mate00/springformlogin/controller/MainController.java
+++ src/main/java/rnd/mate00/springformlogin/controller/MainController.java
@@ -1,11 +1,9 @@
 package rnd.mate00.springformlogin.controller;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import rnd.mate00.springformlogin.user.CustomUserDetails;
 
 @Controller
